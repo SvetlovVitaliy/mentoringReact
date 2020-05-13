a42475e2663166ae86a5b7e5401b12df8e3e7cfe
Vitalii Svetlov <Vitalii_Svetlov@epam.com>
2020-05-13 09:16:03 +0400

    add variable component

--- src/App.js
+++ src/App.js
@@ -4,9 +4,11 @@ import './App.css';
 import Wrapper from './components/Wrapper.jsx';
 
 function App() {
+  const element = (<h1>Hello</h1>);
   return (
     <div className="App">
       <header className="App-header">
+        {element}
         <Wrapper />
         <img src={logo} className="App-logo" alt="logo" />
       </header>
