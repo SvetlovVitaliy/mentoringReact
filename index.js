90d90f9ebaceaa916f5d4b58e854447cb7b17bab
Vitalii Svetlov <Vitalii_Svetlov@epam.com>
2020-05-14 11:53:00 +0400

    add webpack config

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
 import { some } from './src/some';
+import './src/styles/styles.scss';
 
 some();
+
+const template = <p>Hello from react</p>;
+
+ReactDOM.render(template, document.getElementById('root'));
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,34 @@
 'use strict';
+const path = require('path');
+const webpack = require('webpack');
 
 module.exports = {
   entry: './index.js',
+
   output: {
     path: __dirname + '/build',
     filename: 'bundle.js'
-  }
+  },
+
+  devtool: 'source-map',
+
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader']
+      }
+    ]
+  },
+
+  devServer: {
+    contentBase: path.join(__dirname, 'build')
+  },
+
+  watch: true
 }
