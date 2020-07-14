dff6dc42e4eff3b3ade91b10a1940b0d53209bf3
Vitalii Svetlov <Vitalii_Svetlov@epam.com>
2020-07-14 16:41:00 +0400

    add method mount

--- src/setupEnzyme.ts
+++ src/setupEnzyme.ts
@@ -1,4 +1,6 @@
-import { configure } from 'enzyme';
+import Enzyme, { configure, shallow, mount, render } from "enzyme";
 import EnzymeAdapter from 'enzyme-adapter-react-16';
 
 configure({ adapter: new EnzymeAdapter() });
+export { shallow, mount, render };
+export default Enzyme;
